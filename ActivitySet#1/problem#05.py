6e1d0b0b614de4e4d9af7f32e328792332144b48
Khushi prabhu <95675533+Khushi-3@users.noreply.github.com>
2022-05-23 09:18:10 +0000

    modified using functions

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,14 +1,23 @@
-# Functions
-#modify using functions
-def computepay(h, r):
-    if(h>40):
-      pay=(40*r + (h-40)*(1.5*r))
-    else:
-      pay=h*r
-    return pay
+#Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+#Score Grade >= 0.9 A ;>= 0.8 B ; >= 0.7 C ; >= 0.6 D ; < 0.6 F
+#If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+score = float(input("Enter Score: "))
+if(score>=0.9):
+    print("A")
+elif(score>=0.8):
+    print("B")
+elif(score>=0.7):
+    print("C")
+elif(score>=0.6):
+    print("D")
+elif(score<0.6):
+    print("F")
+else:    
+    print("error")
 
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
 
-p = computepay(hrs, rte)
-print("Pay", p)
+
+
+
+
+
